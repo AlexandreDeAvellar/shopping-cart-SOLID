73c4c412e347ca443adca0655c21a1b6b66456ee
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2021-06-27 14:27:59 -0300

    Add funcions: checkout, isEmpty, sendMessage, saveOrder and clear

--- shopping-cart.ts
+++ shopping-cart.ts
@@ -6,6 +6,7 @@ type CartItem = {
 
 export class ShoppingCart {
     private readonly _items: CartItem[] = []
+    private orderStatus: 'open' | 'closed' = "open"
 
     get items(): Readonly<CartItem[]> {
         return this._items
@@ -20,16 +21,44 @@ export class ShoppingCart {
     }
 
     total(): number {
-        return this._items.reduce((acc, val) => acc + val.price, 0)
+        return +this._items.reduce((acc, val) => acc + val.price, 0).toFixed(2)
+    }
+
+    checkou() {
+        if (this.isEmpty()) {
+            console.log('Seu carrinho está vazio.')
+            return
+        }
+
+        this.orderStatus = "closed"
+        this.sendMessage('Seu pedido foi recebido.')
+        this.saveOrder()
+        this.clear()
+    }
+
+    isEmpty() {
+        return this._items.length === 0
+    }
+
+    sendMessage(msg: string): void {
+        console.log('Mensagem enviada:', msg)
+    }
+
+    saveOrder(): void {
+        console.log('Pedido salvo com sucesso.')
+    }
+
+    clear() {
+        this._items.length = 0;
     }
 }
 
 const shoppingCart = new ShoppingCart()
-shoppingCart.addItem({ name: "Camisa", price: 45.00 })
-shoppingCart.addItem({ name: "Calça", price: 55.00 })
-shoppingCart.addItem({ name: "Short", price: 35.00 })
+shoppingCart.addItem({ name: "Camisa", price: 45.362 })
+shoppingCart.addItem({ name: "Calça", price: 55.99 })
+shoppingCart.addItem({ name: "Short", price: 35.853 })
 
-shoppingCart.removeItem(1)
+// shoppingCart.removeItem(1)
 
 console.log(shoppingCart.items)
 console.log(shoppingCart.total())
