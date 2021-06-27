02ca63a8583b6826390551254101a79cfb31b600
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2021-06-27 11:44:39 -0300

    Add function total and create mock with log

--- shopping-cart.ts
+++ shopping-cart.ts
@@ -7,6 +7,10 @@ type CartItem = {
 export class ShoppingCart {
     private readonly _items: CartItem[] = []
 
+    get items(): Readonly<CartItem[]> {
+        return this._items
+    }
+
     addItem(item: CartItem): void {
         this._items.push(item)
     }
@@ -14,4 +18,18 @@ export class ShoppingCart {
     removeItem(index: number): void {
         this._items.splice(index, 1)
     }
+
+    total(): number {
+        return this._items.reduce((acc, val) => acc + val.price, 0)
+    }
 }
+
+const shoppingCart = new ShoppingCart()
+shoppingCart.addItem({ name: "Camisa", price: 45.00 })
+shoppingCart.addItem({ name: "Calça", price: 55.00 })
+shoppingCart.addItem({ name: "Short", price: 35.00 })
+
+shoppingCart.removeItem(1)
+
+console.log(shoppingCart.items)
+console.log(shoppingCart.total())
