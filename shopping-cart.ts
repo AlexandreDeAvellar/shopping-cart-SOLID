2954d284a5836a70476ed508ca2837314c4c5bd2
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2021-06-27 11:29:01 -0300

    Add functions addItem and removeItem

--- shopping-cart.ts
+++ shopping-cart.ts
@@ -1,6 +1,17 @@
-
+type CartItem = {
+    name: string;
+    price: number;
+}
 
 
 export class ShoppingCart {
-    private readonly _items: { nome: string, price: number }[] = []
+    private readonly _items: CartItem[] = []
+
+    addItem(item: CartItem): void {
+        this._items.push(item)
+    }
+
+    removeItem(index: number): void {
+        this._items.splice(index, 1)
+    }
 }
