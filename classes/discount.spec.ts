37b6218d38677da3fe8f1658e2ef4046b4936a87
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2021-07-19 20:38:29 -0300

    add test discount

---
+++ classes/discount.spec.ts
@@ -0,0 +1,24 @@
+import { Discount, NoDiscount, TenPercentDiscount, FiftyPercentDiscount } from './discount'
+
+const createSut = function (className: new () => Discount): Discount {
+    return new className()
+}
+
+describe('Discount', () => {
+    afterEach(e => jest.clearAllMocks())
+
+    it('Should no discount', () => {
+        const sut = createSut(NoDiscount)
+        expect(sut.calculate(10.59)).toBeCloseTo(10.59)
+    })
+    
+    it('Should aplly 10% discount', () => {
+        const sut = createSut(TenPercentDiscount)
+        expect(sut.calculate(10)).toBeCloseTo(9)
+    })
+
+    it('Should aplly 50% discount', () => {
+        const sut = createSut(FiftyPercentDiscount)
+        expect(sut.calculate(10)).toBeCloseTo(5)
+    })
+})
