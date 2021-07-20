31d3e1d874ea9f239772c54cdaab9598ab88b0d9
AlexandreDeAvellar <alexandredeavellar@hotmail.com>
2021-07-19 21:43:34 -0300

    update function create in discount.spec.ts

--- classes/discount.spec.ts
+++ classes/discount.spec.ts
@@ -1,17 +1,18 @@
 import { Discount, NoDiscount, TenPercentDiscount, FiftyPercentDiscount } from './discount'
 
-const createSut = function (className: new () => Discount): Discount {
+const createSut = (className: new () => Discount): Discount => {
     return new className()
 }
 
 describe('Discount', () => {
-    afterEach(e => jest.clearAllMocks())
+    // jest.setTimeout(3000)
+    // afterEach(e => jest.clearAllMocks())
 
     it('Should no discount', () => {
         const sut = createSut(NoDiscount)
         expect(sut.calculate(10.59)).toBeCloseTo(10.59)
     })
-    
+
     it('Should aplly 10% discount', () => {
         const sut = createSut(TenPercentDiscount)
         expect(sut.calculate(10)).toBeCloseTo(9)
